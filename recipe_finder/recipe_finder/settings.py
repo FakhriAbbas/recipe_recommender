a3029721010e213b26f762af2769e9a6c5d1f023
fakhriabbas <fabbas1@uncc.edu>
2020-10-18 00:43:46 -0400

    configration

--- recipe_finder/recipe_finder/settings.py
+++ recipe_finder/recipe_finder/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%v39(yi)#aervyr!fgrx0*hg(+^%1&qnz*c7u9l=b+!6b$lki1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new
+STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+
 
 
